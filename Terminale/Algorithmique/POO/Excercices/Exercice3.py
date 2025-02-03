bdbd0082e2e99a83f7051ae33b04924c34a334d0
Samy Belazzoug <samy.belazzoug@laplateforme.io>
2025-02-03 13:00:32 +0100

    POO - Exerice 3 (Terminale/Algorithmique/POO/Excercices)

--- Terminale/Algorithmique/POO/Excercices/Exercice3.py
+++ Terminale/Algorithmique/POO/Excercices/Exercice3.py
@@ -1,4 +1,28 @@
 """Exercice 3. Classe chaîne étendue"""
 
 class myString:
-    pass
+    def __init__(self,string:str=""):
+        self.string = string
+
+    def append(self,string):
+        self.string += string
+
+    def pop(self,i:int):
+        if i > len(self.string):
+            raise TypeError ("Index out of Range.")
+        copy = ""
+        for j in range(len(self.string)):
+            if j != i:
+                copy += self.string[j]
+        self.string = copy
+                
+    def __repr__(self):
+        return f"{self.string}"
+
+if __name__ == "__main__":
+    s1 = myString("Hello")
+    s2 = myString("Bonjour")
+    s1.append(" World !")
+    s2.pop(2)
+    print(s1)
+    print(s2)
