0a606a3c956e8a21e6e5dd79f6a7a7e5e1ba14e6
Samy Belazzoug <samy.belazzoug@laplateforme.io>
2025-02-26 22:49:07 +0100

    Exercice 1 Done (Premiere/Python/boucles for/exercices

---
+++ Premiere/Python/boucle_for/exercice1.py
@@ -0,0 +1,21 @@
+"""Écrire un algorithme qui affiche les valeurs suivantes (en orange) dans le Terminal : """
+
+#Question 1
+
+for i in range(10,17):
+    print(i)
+
+#Question 2
+
+for i in range(10,25,2):
+    print(i)
+
+#Question 3
+
+for i in range(30,37,1):
+    print(i)
+
+#Question 4
+
+for i in range(36,31,-1):
+    print(i)
